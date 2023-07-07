931fe982973e8d0e54ac8857b329623fd013d946
6uf <84757238+suffz@users.noreply.github.com>
2023-07-07 17:59:04 -0300

    Remove the errors from groupsv2

--- blox.go
+++ blox.go
@@ -12,39 +12,12 @@ func Config(key string) App {
 	return App{Key: key}
 }
 
-func CheckForPremium(Data map[string]any) (Return []PremiumGroups) {
-	if info, ok := Data["resolved"].(map[string]any)["roblox"]; ok {
-		for _, value := range info.(map[string]interface{})["groupsv2"].(map[string]interface{}) {
-			G := value.(map[string]interface{})["group"].(map[string]interface{})
-			R := value.(map[string]interface{})["role"].(map[string]interface{})
-			Data := PremiumGroups{
-				Group: Group{
-					ID:               int(G["id"].(float64)),
-					Name:             G["name"].(string),
-					MemberCount:      int(G["memberCount"].(float64)),
-					HasVerifiedBadge: G["hasVerifiedBadge"].(bool),
-				},
-				Role: Role{
-					ID:   int(R["id"].(float64)),
-					Name: R["name"].(string),
-					Rank: int(R["rank"].(float64)),
-				},
-			}
-			Return = append(Return, Data)
-		}
-	}
-	return
-}
-
 func (k *App) ServerDiscordToRoblox(DiscordID, GuildID string) (data Roblox, err Error) {
 	if resp, e := http.DefaultClient.Do(genHttp(must(http.NewRequest("GET", fmt.Sprintf(ServerDiscordToRoblox, GuildID, DiscordID), nil)), []headers{{Name: "Authorization", Value: k.Key}})); e == nil {
 		res, _ := io.ReadAll(resp.Body)
 		switch resp.StatusCode {
 		case 200:
 			json.Unmarshal(res, &data)
-			var Check map[string]any = make(map[string]any)
-			json.Unmarshal(res, &Check)
-			data.Resolved.Roblox.Groupsv2 = CheckForPremium(Check)
 			return
 		case 404:
 			json.Unmarshal(res, &err)
@@ -99,11 +72,6 @@ func (k *App) GlobalDiscordToRoblox(DiscordID string) (data Roblox, err Error) {
 		switch resp.StatusCode {
 		case 200:
 			json.Unmarshal(res, &data)
-
-			var Check map[string]any = make(map[string]any)
-			json.Unmarshal(res, &Check)
-			data.Resolved.Roblox.Groupsv2 = CheckForPremium(Check)
-
 			return
 		case 404:
 			json.Unmarshal(res, &err)
